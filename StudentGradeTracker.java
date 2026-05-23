bbf14260f40019d12cb1cf0c5792b32e2f67d53c
psainathreddy945-bit <psainathreddy945@gmail.com>
2026-05-23 15:59:19 +0530

    Add files via upload

---
+++ StudentGradeTracker.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Student {
+    String name;
+    int marks;
+
+    Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+}
+
+public class StudentGradeTracker {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Student> students = new ArrayList<>();
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.println("\nStudent " + (i + 1));
+
+            System.out.print("Enter student name: ");
+            String name = sc.nextLine();
+
+            System.out.print("Enter marks: ");
+            int marks = sc.nextInt();
+            sc.nextLine();
+
+            students.add(new Student(name, marks));
+        }
+
+        int total = 0;
+        int highest = students.get(0).marks;
+        int lowest = students.get(0).marks;
+
+        for (Student s : students) {
+
+            total += s.marks;
+
+            if (s.marks > highest) {
+                highest = s.marks;
+            }
+
+            if (s.marks < lowest) {
+                lowest = s.marks;
+            }
+        }
+
+        double average = (double) total / n;
+
+        System.out.println("\n===== STUDENT REPORT =====");
+
+        for (Student s : students) {
+            System.out.println("Name: " + s.name + " | Marks: " + s.marks);
+        }
+
+        System.out.println("\nAverage Marks: " + average);
+        System.out.println("Highest Marks: " + highest);
+        System.out.println("Lowest Marks: " + lowest);
+
+        sc.close();
+    }
+}
